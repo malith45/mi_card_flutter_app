0f5e01cc9cd195a3f402cdda8892e0e6799cf4e9
Malith Kumaranayaka <127102703+malith45@users.noreply.github.com>
2024-09-08 17:55:02 +0530

    Made a stateless widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
